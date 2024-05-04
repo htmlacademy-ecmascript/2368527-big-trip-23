86f913807c1d3dc442affa85f44af703f2be3164
L0rdxD <lorddirolll@gmail.com>
2024-05-04 17:06:43 +0300

    Делает рефакторинг кода

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
+import { render } from './render';
 import FiltersView from './view/filters';
-import SortView from './view/sort';
 import EventPresenter from './presenter/event-presenter';
 
 const tripControlsFiltersEl = document.querySelector('.trip-controls__filters');
 const tripEventsEl = document.querySelector('.trip-events');
-const eventPresenter = new EventPresenter();
-
-tripControlsFiltersEl.append(new FiltersView().getElement());
-tripEventsEl.append(new SortView().getElement());
+const eventPresenter = new EventPresenter({
+  containerEl: tripEventsEl,
+});
+render(new FiltersView(), tripControlsFiltersEl);
 
 eventPresenter.init();
--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -1,19 +1,50 @@
+import { render } from '../render';
 import EventsListView from '../view/events-list';
 import EventCreateView from '../view/event-create';
 import EventEditView from '../view/event-edit';
 import EventItemView from '../view/event-item';
+import SortingView from '../view/sorting';
 
 export default class EventPresenter {
+  constructor({ containerEl }) {
+    this.containerEl = containerEl;
+  }
+
+  renderSortingView() {
+    this.sortingView = new SortingView();
+    render(this.sortingView, this.containerEl);
+  }
+
+  renderEventsListView() {
+    this.eventsListView = new EventsListView();
+    render(this.eventsListView, this.containerEl);
+  }
+
+  renderEventEditView() {
+    this.eventEditView = new EventEditView();
+    render(this.eventEditView, this.eventsListView.getElement());
+  }
+
+  renderEventCreateView() {
+    this.eventCreateView = new EventCreateView();
+    render(this.eventCreateView, this.eventsListView.getElement());
+  }
+
+  renderEventItemView() {
+    this.eventItemView = new EventItemView();
+    render(this.eventItemView, this.eventsListView.getElement());
+  }
+
   init() {
-    const tripEventsEl = document.querySelector('.trip-events');
-    const eventsListEl = new EventsListView().getElement();
+    this.renderSortingView();
+    this.renderEventsListView();
+    this.renderEventEditView();
+    this.renderEventCreateView();
 
-    eventsListEl.append(new EventEditView().getElement());
-    eventsListEl.append(new EventCreateView().getElement());
-    Array.from({ length: 3 }, () => {
-      eventsListEl.append(new EventItemView().getElement());
-    });
+    for (let i = 0; i < 3; i++) {
+      this.renderEventItemView();
+    }
 
-    tripEventsEl.append(eventsListEl);
+    render(this.eventsListView, this.containerEl);
   }
 }

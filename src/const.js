40c422a65841aae61e012944470f038b4aedfb6a
L0rdxD <lorddirolll@gmail.com>
2024-05-08 14:05:30 +0300

    Создает словарь с типом событий

---
+++ src/const.js
@@ -0,0 +1,13 @@
+const EventType = {
+  TAXI: 'taxi',
+  BUS: 'bus',
+  TRAIN: 'train',
+  SHIP: 'ship',
+  DRIVE: 'drive',
+  FLIGHT: 'flight',
+  CHECK_IN: 'check-in',
+  SIGHTSEEING: 'sightseeing',
+  RESTAURANT: 'restaurant',
+};
+
+export { EventType };

a570d5dabeaa0684b3671ae03b7a730926e5df6a
L0rdxD <lorddirolll@gmail.com>
2024-05-10 11:55:22 +0300

    TrainBangkok не выводит данные и возможная ошибка со звездами

--- src/main.js
+++ src/main.js
@@ -1,16 +1,5 @@
 import { render } from './render';
-<<<<<<< Updated upstream
-import FiltersView from './view/filters';
-import EventPresenter from './presenter/event-presenter';
-import WaypointsModel from './model/waypoints-model';
 
-const tripControlsFiltersEl = document.querySelector('.trip-controls__filters');
-const tripEventsEl = document.querySelector('.trip-events');
-const waypointsModel = new WaypointsModel();
-const eventPresenter = new EventPresenter({
-  containerEl: tripEventsEl,
-  waypointsModel,
-=======
 import FiltersView from './view/filters.js';
 import ListPresenter from './presenter/list-presenter.js';
 import PointsModel from './model/points-model.js';
@@ -22,7 +11,6 @@ const pointsModel = new PointsModel();
 const presenter = new ListPresenter({
   countainer: siteTripEventsElement,
   pointsModel,
->>>>>>> Stashed changes
 });
 
 render(new FiltersView(), siteHeaderElement);
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,5 @@
 import dayjs from 'dayjs';
-<<<<<<< Updated upstream
-
-const getRandomPositiveInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-=======
 import { mockOffers } from './mock/points';
->>>>>>> Stashed changes
 
 const DATE_FORMAT = 'D MMMM';
 const TIME_FORMAT = 'HH:mm';
@@ -21,45 +13,6 @@ function getRandomInt(maxInt) {
   return Math.floor(Math.random() * maxInt);
 }
 
-<<<<<<< Updated upstream
-const getDateStringFromDate = (date) => dayjs(date).format('YYYY-MM-DD');
-
-const getTimeStringFromDate = (date) => dayjs(date).format('HH:mm');
-
-const humanizeDay = (date) => dayjs(date).format('MMM D');
-
-const printDuration = (dateFrom, dateTo) => {
-  const dateStart = dayjs(dateFrom);
-  const dateEnd = dayjs(dateTo);
-  const diff = dateEnd.diff(dateStart);
-  const diffDuration = dayjs.duration(diff);
-
-  const daysDuration = diffDuration.days();
-  const hoursDuration = diffDuration.hours();
-  const minutesDuration = diffDuration.minutes();
-  const days = `${daysDuration.toString().padStart(2, '0')}D`;
-  const hours = `${hoursDuration.toString().padStart(2, '0')}H`;
-  const minutes = `${minutesDuration.toString().padStart(2, '0')}M`;
-
-  if (daysDuration) {
-    return `${days} ${hours} ${minutes}`;
-  }
-
-  if (hoursDuration) {
-    return `${hours} ${minutes}`;
-  }
-
-  return minutes;
-};
-
-export {
-  getRandomArrayItem,
-  capitaliseFirstLetter,
-  getDateStringFromDate,
-  getTimeStringFromDate,
-  humanizeDay,
-  printDuration,
-=======
 function humanizeTaskDueDate(dueDate) {
   return dueDate ? dayjs(dueDate).format(DATE_FORMAT) : '';
 }
@@ -105,5 +58,4 @@ export {
   isFavoriteStyle,
   getDateTime,
   getOffersTypeLength,
->>>>>>> Stashed changes
 };

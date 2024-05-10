926180c173ff734064b54f1bc9c5cd83ac520dce
L0rdxD <lorddirolll@gmail.com>
2024-05-10 15:04:30 +0300

    исправление ошибок

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,6 +1,6 @@
 import { getRandomPoint } from '../mock/points';
 
-const POINT_COUNT = 3;
+const POINT_COUNT = 4;
 
 export default class PointsModel {
   points = Array.from({ length: POINT_COUNT }, getRandomPoint);

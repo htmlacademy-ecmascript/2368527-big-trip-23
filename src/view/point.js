e5f05bac86cf5bcb804cf61f9825bb0bf60e35eb
L0rdxD <lorddirolll@gmail.com>
2024-05-11 12:19:36 +0300

    Финальные исправления

--- src/view/point.js
+++ src/view/point.js
@@ -44,7 +44,7 @@ function createPointTemplate(point) {
       <div class="event__type">
       <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
-      <h3 class="event__title">${type}${objDestination.name}</h3>
+      <h3 class="event__title">${type} ${objDestination.name}</h3>
       <div class="event__schedule">
       <p class="event__time">
       <time class="event__start-time" datetime="2019-03-18T14:30">${timeFrom}</time>

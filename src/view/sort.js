de718881718315f3bab94cdf09c7eafc6ee3fcb0
L0rdxD <lorddirolll@gmail.com>
2024-06-18 16:19:51 +0300

    Сортировка заработала

--- src/view/sort.js
+++ src/view/sort.js
@@ -2,13 +2,13 @@ import { SortType } from '../const.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 const DISABLED_SORT_TYPES = [SortType.EVENT, SortType.OFFERS];
+const SORT_PREFIX = 'sort-';
 
 const createSortTemplate = (activeSorteTipe) =>
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
   ${Object.values(SortType)
     .map(
-      (sortType) =>
-        `<div class="trip-sort__item  trip-sort__item--${sortType}">
+      (sortType) => `<div class="trip-sort__item  trip-sort__item--${sortType}">
       <input id="sort-${sortType}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort"
       value="sort-${sortType}"${sortType === activeSorteTipe ? 'checked' : ''}
       ${DISABLED_SORT_TYPES.includes(sortType) ? 'disabled' : ''}>
@@ -35,11 +35,7 @@ export default class SortView extends AbstractView {
   }
 
   #sortTypeChangeHandler = (evt) => {
-    if (evt.target.tagName !== 'A') {
-      return;
-    }
-
-    evt.preventDefault();
-    this.#handleSortTypeChange(evt.target.dataset.sortType);
+    const sortType = evt.target.id.replace(SORT_PREFIX, '');
+    this.#handleSortTypeChange(sortType);
   };
 }
